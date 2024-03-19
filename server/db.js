5e4440a91928ba0a58fc9d67f9e4b07e48b9c38a
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2024-03-19 00:19:42 -0400

    created tables

--- server/db.js
+++ server/db.js
@@ -1,6 +1,30 @@
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_store');
 
+const createTables = async()=> {
+    const SQL = `
+      DROP TABLE IF EXISTS favorite;
+      DROP TABLE IF EXISTS users;
+      DROP TABLE IF EXISTS products;
+      CREATE TABLE users(
+        id UUID PRIMARY KEY,
+        username VARCHAR(100) UNIQUE NOT NULL,
+        password VARCHAR(255) UNIQUE NOT NULL
+      );
+      CREATE TABLE products(
+        id UUID PRIMARY KEY,
+        name VARCHAR(100) NOT NULL
+      );
+      CREATE TABLE favorite(
+        id UUID PRIMARY KEY,
+        product_id UUID REFERENCES products(id) NOT NULL,
+        user_id UUID REFERENCES users(id) NOT NULL,
+        CONSTRAINT unique_product_user UNIQUE (product_id, user_id)
+      );
+    `;
+    await client.query(SQL);
+  };
+
 module.exports = {
   client
 };
--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
-const { client } = require('./db');
+const { 
+    client,
+    createTables,
+    createProduct,
+    createUser,
+    fetchUsers,
+    fetchProducts,
+    fetchFavorites,
+    createFavorites,
+    destroyFavorite
+ } = require('./db');
+const express = require('express');
+const app = express();
+app.use(express.json());
 
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
+  await createTables();
+  console.log('tables created');
 };
 
 init();
