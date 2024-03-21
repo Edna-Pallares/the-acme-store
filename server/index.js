a97cb2bab2a8df4afa1b205a1b82c18fc39ac5c9
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2024-03-21 19:13:19 -0400

    updates

--- server/index.js
+++ server/index.js
@@ -14,10 +14,23 @@ const app = express();
 app.use(express.json());
 
 const init = async()=> {
-  await client.connect();
-  console.log('connected to database');
-  await createTables();
-  console.log('tables created');
-};
-
-init();
+    await client.connect();
+    console.log('connected to database');
+    await createTables();
+    console.log('tables created');
+    const [ean, tom, lee, chips, books, drinks, pens] = await Promise.all([
+      createUser({ username: 'ean', password: 's3cr3t' }),
+      createUser({ username: 'tom', password: 's3cr3t!!' }),
+      createUser({ username: 'lee', password: 'shhh' }),
+      createProduct({ name: 'chips'}),
+      createProduct({ name: 'books'}),
+      createProduct({ name: 'drinks'}),
+      createProduct({ name: 'pens'}),
+    ]);
+    console.log(ean.id);
+    console.log(pens.id)
+  
+  };
+  
+  init();
+  
