3ecc99bc2372f8d56df0badb3dfff80570b1dad7
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2024-03-24 22:36:59 -0400

    create methods

--- server/db.js
+++ server/db.js
@@ -1,8 +1,12 @@
-const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/the_acme_store');
+const pg = require("pg");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/the_acme_store"
+);
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
 
-const createTables = async()=> {
-    const SQL = `
+const createTables = async () => {
+  const SQL = `
       DROP TABLE IF EXISTS favorite;
       DROP TABLE IF EXISTS users;
       DROP TABLE IF EXISTS products;
@@ -22,9 +26,64 @@ const createTables = async()=> {
         CONSTRAINT unique_product_user UNIQUE (product_id, user_id)
       );
     `;
-    await client.query(SQL);
-  };
+  await client.query(SQL);
+};
+
+const createUser = async ({ username, password }) => {
+  const SQL = `
+    INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *
+    `;
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    username,
+    await bcrypt.hash(password, 5),
+  ]);
+  return response.rows[0];
+};
+const createProduct = async ({ name }) => {
+  const SQL = `
+    INSERT INTO products(id, name) VALUES($1, $2) RETURNING *
+    `;
+  const response = await client.query(SQL, [uuid.v4(), name]);
+  return response.rows[0];
+};
+const createFavorite = async ({ user_id, product_id})=> {
+  const SQL = `
+  INSERT INTO user_favorites(id, product_id, user_id) VALUES($1, $2, $3) RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), user_id, product_id]);
+  return response.rows[0];
+}
+const fetchUsers = async () => {
+  const SQL = `
+    SELECT * FROM users;
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+const fetchProducts = async () => {
+  const SQL = `
+    SELECT * FROM products;
+    `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+const fetchFavorites = async () => {
+  const SQL = `
+  SELECT * FROM user_favorites
+  WHERE user_id = $1
+  `;
+  const response = await client.query(SQL, [ uuid ]);
+  return response.rows;
+}
 
 module.exports = {
-  client
+  client,
+  createTables,
+  createUser,
+  createProduct,
+  createFavorite,
+  fetchUsers,
+  fetchProducts,
+  fetchFavorites
 };
--- server/index.js
+++ server/index.js
@@ -1,36 +1,46 @@
-const { 
-    client,
-    createTables,
-    createProduct,
-    createUser,
-    fetchUsers,
-    fetchProducts,
-    fetchFavorites,
-    createFavorites,
-    destroyFavorite
- } = require('./db');
-const express = require('express');
+const {
+  client,
+  createTables,
+  createProduct,
+  createUser,
+  fetchUsers,
+  fetchProducts,
+  fetchFavorites,
+  createFavorite,
+  destroyFavorite,
+} = require("./db");
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-const init = async()=> {
-    await client.connect();
-    console.log('connected to database');
-    await createTables();
-    console.log('tables created');
-    const [ean, tom, lee, chips, books, drinks, pens] = await Promise.all([
-      createUser({ username: 'ean', password: 's3cr3t' }),
-      createUser({ username: 'tom', password: 's3cr3t!!' }),
-      createUser({ username: 'lee', password: 'shhh' }),
-      createProduct({ name: 'chips'}),
-      createProduct({ name: 'books'}),
-      createProduct({ name: 'drinks'}),
-      createProduct({ name: 'pens'}),
-    ]);
-    console.log(ean.id);
-    console.log(pens.id)
-  
-  };
-  
-  init();
-  
+const init = async () => {
+  await client.connect();
+  console.log("connected to database");
+  await createTables();
+  console.log("tables created");
+  const [ean, tom, lee, chips, books, drinks, pens] = await Promise.all([
+    createUser({ username: "ean", password: "s3cr3t" }),
+    createUser({ username: "tom", password: "s3cr3t!!" }),
+    createUser({ username: "lee", password: "shhh" }),
+    createProduct({ name: "chips" }),
+    createProduct({ name: "books" }),
+    createProduct({ name: "drinks" }),
+    createProduct({ name: "pens" }),
+  ]);
+  const users = await fetchUsers();
+  console.log(users);
+
+  const products = await fetchProducts();
+  console.log(products);
+
+  const Favorites = await Promise.all([
+    createFavorite({ user_id: ean.id, product_id: chips.id }),
+    createFavorite({ user_id: ean.id, product_id: books.id }),
+    createFavorite({ user_id: tom.id, product_id: drinks.id }),
+    createFavorite({ user_id: lee.id, product_id: pens.id }),
+  ]);
+
+  console.log(await fetchFavorites(moe.id));
+};
+
+init();
